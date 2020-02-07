df9221e2d19ab77e7074b8cf9bbe2f25f08b7241
irul <irul_sylva@yahoo.com>
2020-02-07 13:45:56 +0700

    Serving Images

--- look/app.py
+++ look/app.py
@@ -2,16 +2,16 @@ import os
 
 import falcon
 
-from .images import ImageStore, Resource
+import look.images as images
 
 def create_app(image_store):
-    image_resource = Resource(image_store)
     api = falcon.API()
-    api.add_route('/images', image_resource)
+    api.add_route('/images', images.Collection(image_store))
+    api.add_route('/images/{name}', images.Item(image_store))
     return api
 
 
 def get_app():
     storage_path = os.environ.get('LOOK_STORAGE_PATH', '.')
-    image_store = ImageStore(storage_path)
+    image_store = images.ImageStore(storage_path)
     return create_app(image_store)
--- look/images.py
+++ look/images.py
@@ -1,5 +1,6 @@
 import io
 import os
+import re
 import uuid
 import mimetypes
 import falcon
@@ -8,7 +9,7 @@ import json
 import msgpack
 
 
-class Resource(object):
+class Collection(object):
 
     def __init__(self, image_store):
         self._image_store = image_store
@@ -34,13 +35,23 @@ class Resource(object):
         resp.status = falcon.HTTP_201
         resp.location = '/images/' + name
 
+class Item(object):
+
+    def __init__(self, image_store):
+        self._image_store = image_store
+
+    def on_get(self, req, resp, name):
+        resp.content_type = mimetypes.guess_type(name)[0]
+        resp.stream, resp.content_length = self._image_store.open(name)
+
 
 class ImageStore(object):
 
     _CHUNK_SIZE_BYTES = 4096
+    _IMAGE_NAME_PATTERN = re.compile(
+        '[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\.[a-z]{2,4}$'
+    )
 
-    # Note the use of dependency injection for standard library
-    # methods. We'll use these later to avoid monkey-patching.
     def __init__(self, storage_path, uuidgen=uuid.uuid4, fopen=io.open):
         self._storage_path = storage_path
         self._uuidgen = uuidgen
@@ -60,3 +71,14 @@ class ImageStore(object):
                 image_file.write(chunk)
 
         return name
+
+    def open(self, name):
+        # Always validate untrusted input!
+        if not self._IMAGE_NAME_PATTERN.match(name):
+            raise IOError('File not found')
+
+        image_path = os.path.join(self._storage_path, name)
+        stream = self._fopen(image_path, 'rb')
+        content_length = os.path.getsize(image_path)
+
+        return stream, content_length
