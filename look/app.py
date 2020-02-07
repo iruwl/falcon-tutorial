b346f00aa6373b7bf04bc33e3f70a86b864872a3
irul <irul_sylva@yahoo.com>
2020-02-07 13:34:28 +0700

    Functional tests

--- look/app.py
+++ look/app.py
@@ -1,3 +1,5 @@
+import os
+
 import falcon
 
 from .images import ImageStore, Resource
@@ -10,5 +12,6 @@ def create_app(image_store):
 
 
 def get_app():
-    image_store = ImageStore('.')
+    storage_path = os.environ.get('LOOK_STORAGE_PATH', '.')
+    image_store = ImageStore(storage_path)
     return create_app(image_store)
