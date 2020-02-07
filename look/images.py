9e00c46b49435ebbc32e4ae7ec876d16bd881372
irul <irul_sylva@yahoo.com>
2020-02-07 11:20:11 +0700

    Request and Response Objects

--- look/images.py
+++ look/images.py
@@ -1,3 +1,7 @@
+import io
+import os
+import uuid
+import mimetypes
 import falcon
 
 import json
@@ -6,6 +10,13 @@ import msgpack
 
 class Resource(object):
 
+    _CHUNK_SIZE_BYTES = 4096
+
+    # The resource object must now be initialized with a path used during POST
+    def __init__(self, storage_path):
+        self._storage_path = storage_path
+
+    # This is the method we implemented before
     def on_get(self, req, resp):
         doc = {
             'images': [
@@ -23,3 +34,19 @@ class Resource(object):
         resp.content_type = falcon.MEDIA_MSGPACK
 
         resp.status = falcon.HTTP_200
+
+    def on_post(self, req, resp):
+        ext = mimetypes.guess_extension(req.content_type)
+        name = '{uuid}{ext}'.format(uuid=uuid.uuid4(), ext=ext)
+        image_path = os.path.join(self._storage_path, name)
+
+        with io.open(image_path, 'wb') as image_file:
+            while True:
+                chunk = req.stream.read(self._CHUNK_SIZE_BYTES)
+                if not chunk:
+                    break
+
+                image_file.write(chunk)
+
+        resp.status = falcon.HTTP_201
+        resp.location = '/images/' + name
